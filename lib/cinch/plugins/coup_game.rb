b50a5d964aa7c763f193225b3be7d3d8c56d33d1
Peter Tseng <pht24@cornell.edu>
2014-01-31 11:27:23 -0500

    switch_cards: Implement

--- lib/cinch/plugins/coup_game.rb
+++ lib/cinch/plugins/coup_game.rb
@@ -345,12 +345,39 @@ module Cinch
       def switch_cards(m, choice)
         if @game.started? && @game.has_player?(m.user)
           player = @game.find_player(m.user)
+          facedown_indices = [0, 1].select { |i|
+            player.characters[i].face_down?
+          }
+          facedowns = facedown_indices.collect { |i| player.characters[i] }
+          cards_to_return = facedowns + @drawn_cards
+
           if @game.current_turn.waiting_for_decision? && @game.current_turn.decider == player
+            choice = choice.to_i
+            if 1 <= choice && choice <= @character_options.size
+              new_hand = @character_options[choice - 1]
+              # Remove the new hand from cards_to_return
+              new_hand.each { |c|
+                card_index = cards_to_return.index(c)
+                cards_to_return.delete_at(card_index)
+              }
 
-            character = player.switch_cards(position.to_i)
-            Channel(@channel_name).send "#{m.user.nick} turns a #{character} face up."
+              puts "New hand is #{new_hand.to_a}"
+              puts "Returning #{cards_to_return.to_a}"
 
-            self.start_new_turn
+              facedown_indices.each_with_index { |i, j|
+                # If they have two facedowns, this will switch both.
+                # If they have one facedown,
+                # this will switch their one facedown with the card they picked
+                player.switch_character(new_hand[j], i)
+              }
+
+              @game.shuffle_into_deck(*cards_to_return)
+              Channel(@channel_name).send "#{m.user.nick} shuffles two cards into the Court Deck."
+
+              self.start_new_turn
+            else
+              User(player.user).send "#{choice} is not a valid choice"
+            end
           end
         end
       end
