d0a0e060f962dc926e3ef16ed1aa3102c9894f5e
Peter Tseng <pht24@cornell.edu>
2014-02-07 18:52:00 -0500

    rules: Replace spammy text with link to BGG image

--- lib/cinch/plugins/coup_game.rb
+++ lib/cinch/plugins/coup_game.rb
@@ -1213,18 +1213,9 @@ module Cinch
       end
 
       def rules(m)
-        User(m.user).send "You are head of a family in an Italian city-state, a city run by a weak and corrupt court. You need to manipulate, bluff and bribe your way to power. Your object is to destroy the influence of all the other families, forcing them into exile. Only one family will survive..."
-        User(m.user).send "In Coup, you want to be the last player with influence in the game, with influence being represented by face-down character cards in your playing area."
-        User(m.user).send "Each player starts the game with two coins and two influence - i.e., two face-down character cards; the fifteen card deck consists of three copies of five different characters, each with a unique set of powers:"
-        User(m.user).send "  * Duke: Take three coins from the treasury. Block someone from take foreign aid."
-        User(m.user).send "  * Assassin: Pay three coins and try to assassinate another player's character."
-        User(m.user).send "  * Contessa: Block an assassination attempt."
-        User(m.user).send "  * Captain: Take two coins from another player, or block someone from stealing coins from you."
-        User(m.user).send "  * Ambassador: Draw two character cards from the Court (the deck), choose which (if any) to exchange with your face-down characters, then return two. Block someone from stealing coins from you."
-        User(m.user).send "On your turn, you can take any of the actions listed above, regardless of which characters you actually have in front of you, or you can take one of three other actions:"
-        User(m.user).send "  * Income: Take one coin from the treasury."
-        User(m.user).send "  * Foreign aid: Take two coins from the treasury."
-        User(m.user).send "  * Coup: Pay seven coins and launch a coup against an opponent, forcing that player to lose an influence. (If you have ten coins, you must take this action.)"
+        User(m.user).send "Each player starts the game with two coins and two influence - i.e., two face-down character cards; the fifteen card deck consists of three copies of five different characters, each with a unique set of powers."
+        m.user.send("http://boardgamegeek.com/image/1812508/coup")
+        m.user.send("On your turn, you can take any character's action, regardless of which characters you actually have in front of you, or you can take one three actions that require no character: Income, Foreign Aid, or Coup (if you have ten coins, you must Coup).")
         User(m.user).send "When you take one of the character actions - whether actively on your turn, or defensively in response to someone else\'s action - that character\'s action automatically succeeds unless an opponent challenges you. In this case, if you can\'t reveal the appropriate character, you lose an influence, turning one of your characters face-up. Face-up characters cannot be used, and if both of your characters are face-up, you\'re out of the game."
         User(m.user).send "If you do have the character in question, you reveal it, the opponent loses an influence, then you shuffle that character into the deck and draw a new one, perhaps getting the same character again and perhaps not."
         User(m.user).send "The last player to still have influence - that is, a face-down character - wins the game!"
