petertseng/cinch-coupgame

commit 913e4144bd9fb8ef5f99f2c11c9f6baf884667dc

Player: Use attr_reader where possible

lib/cinch/plugins/core/player.rb
@@ -4,14 +4,14 @@
 
 class Player
 
-  attr_accessor :user, :characters, :coins
+  attr_reader :user, :characters, :coins
   attr_reader :side_cards
   attr_accessor :faction
 
   def initialize(user)
-    self.user = user
-    self.characters = []
-    self.coins = 0
+    @user = user
+    @characters = []
+    @coins = 0
 
     # The four cards set aside in a 2p game.
     @side_cards = []
@@ -66,16 +66,16 @@ class Player
   end
 
   def give_coins(amount)
-    self.coins += amount
+    @coins += amount
   end
 
   def take_coins(amount)
     if self.coins < amount
       taken = self.coins
-      self.coins = 0
+      @coins = 0
       return taken
     end
-    self.coins -= amount
+    @coins -= amount
     amount
   end
 

commit ad4da57b6ef77c506da238f56749502dc061050d

Player: make user accessible
It's used for replacing
broke it in 913e4144bd9fb8ef5f99f2c11c9f6baf884667dc but it looks like
nobody noticed yet...

lib/cinch/plugins/core/player.rb
@@ -4,9 +4,8 @@
 
 class Player
 
-  attr_reader :user, :characters, :coins
-  attr_reader :side_cards
-  attr_accessor :faction
+  attr_accessor :user, :faction
+  attr_reader :characters, :coins, :side_cards
 
   def initialize(user)
     @user = user